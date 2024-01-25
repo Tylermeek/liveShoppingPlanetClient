5bfdae58bd8237fa79f09ee315a5a67610b4b5a4
Tyler <1755276345@qq.cpm>
2024-01-26 01:25:58 +0800

    feat: 基本视屏框架搭建

--- src/screens/Live/index.tsx
+++ src/screens/Live/index.tsx
@@ -1,52 +1,74 @@
 import { Image, Text } from "@rneui/themed";
 import { getLiveSwiperBanner } from "axios/api/live";
+import { Video } from "expo-av";
 import React, { useEffect, useState } from "react";
 import { Dimensions, ScrollView, StatusBar, StyleSheet, TouchableOpacity, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Carousel from "react-native-reanimated-carousel";
 import { LiveSwiperInfo } from "types/info";
 import { scaleSizeH, scaleSizeW } from "utlis/scaleSize";
-import LivingFollowingList from "./LivingFollowingList";
 
 const Live: React.FC = () => {
     const containerWidth = Dimensions.get('window').width
-    const containerHeight = Dimensions.get('window').height
-    const [swiperList, setSwiperList] = useState<LiveSwiperInfo[]>([])
+    const containerHeight = Dimensions.get('window').height -scaleSizeH(40)
+    const [liveList, setLiveList] = useState<any[]>([])
     function handlePress(item: LiveSwiperInfo): void {
         // throw new Error("Function not implemented.");
         console.log(item.liveId);
 
     }
+    const test1 = require("../../../assets/test1.mp4")
+    const test2 = require("../../../assets/test2.mp4")
+    const test3 = require("../../../assets/test3.mp4")
+    const test4 = require("../../../assets/test4.mp4")
 
     useEffect(() => {
-        getLiveSwiperBanner()
-            .then((res) => {
-                setSwiperList(res.data)
-            })
+        // getLiveSwiperBanner()
+        //     .then((res) => {
+        //         setSwiperList(res.data)
+        //     })
+        setLiveList([
+            {
+                source: test1,
+                cover: "https://source.unsplash.com/random?sig=1"
+            },
+            {
+                source: test2,
+                cover: "https://source.unsplash.com/random?sig=2"
+            },
+            {
+                source: test3,
+                cover: "https://source.unsplash.com/random?sig=3"
+            },
+            {
+                source: test4,
+                cover: "https://source.unsplash.com/random?sig=4"
+            },
+        ])
     }, [])
 
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-
             <ScrollView style={{ flex: 1 }}>
-                <GestureHandlerRootView style={{ flex: 1 }}>
-                    <Carousel
-                        loop
-                        width={containerWidth}
-                        height={containerHeight}
-                        vertical
-                        autoPlay={false}
-                        pagingEnabled={false}
-                        snapEnabled
-                        data={swiperList}
-                        scrollAnimationDuration={1500}
-                        renderItem={({ item, index }) => (
-                            <TouchableOpacity key={item.id} style={{ flex: 1 }} onPress={() => handlePress(item)}>
-                                <Image source={{ uri: item.image }} style={{ height: "100%", width: "100%" }}></Image>
-                            </TouchableOpacity>
-                        )}
-                    />
-                </GestureHandlerRootView>
+                <Carousel
+                    width={containerWidth}
+                    height={containerHeight}
+                    vertical
+                    autoPlay={false}
+                    pagingEnabled={false}
+                    snapEnabled
+                    data={liveList}
+                    scrollAnimationDuration={1500}
+                    renderItem={({ item, index }) => (
+                        <Video
+                            source={item.source}
+                            // shouldPlay
+                            useNativeControls
+                            style={{ width: "100%", height: "100%", backgroundColor: "grey" }}
+                        />
+                    )}
+                />
+
             </ScrollView>
 
         </GestureHandlerRootView>
