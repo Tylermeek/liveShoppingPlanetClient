1590e5ede86d542f28e2d04f7df33713fd6f48cd
TylerTheDuck <102410562+Tylermeek@users.noreply.github.com>
2024-03-19 15:19:49 +0800

    feat: state持久化

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import RootStack from "navigators/RootStack";
 import { StyleSheet, View } from "react-native";
 import { createTheme, ThemeProvider } from "@rneui/themed";
 import { Provider } from "react-redux";
-import { store } from "store";
+import { store, persistor } from "store";
 import { scaleSizeW } from "utlis/scaleSize";
-import 'dayjs/locale/zh-cn'
-import * as dayjs from 'dayjs'
+import "dayjs/locale/zh-cn";
+import * as dayjs from "dayjs";
+import { PersistGate } from "redux-persist/integration/react";
 
-dayjs.locale('zh-cn')
+dayjs.locale("zh-cn");
 
 export default function App() {
   const theme = createTheme({
@@ -31,37 +32,39 @@ export default function App() {
       Text: {
         h1Style: {
           fontSize: scaleSizeW(18),
-          fontWeight: "900"
+          fontWeight: "900",
         },
         h2Style: {
           fontSize: scaleSizeW(15),
-          fontWeight: "700"
+          fontWeight: "700",
         },
         h3Style: {
           fontSize: scaleSizeW(13),
-          fontWeight: "600"
+          fontWeight: "600",
         },
         h4Style: {
           fontSize: scaleSizeW(12),
-          fontWeight: "400"
-        }
-      }
+          fontWeight: "400",
+        },
+      },
     },
   });
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <StatusBar
-          animated={true}
-          hidden={false}
-          translucent={true}
-          backgroundColor="transparent"
-          barStyle={"default"}
-          showHideTransition={"fade"}
-          networkActivityIndicatorVisible={true}
-        />
-        <RootStack></RootStack>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <StatusBar
+            animated={true}
+            hidden={false}
+            translucent={true}
+            backgroundColor="transparent"
+            barStyle={"default"}
+            showHideTransition={"fade"}
+            networkActivityIndicatorVisible={true}
+          />
+          <RootStack></RootStack>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../slice/counter/counterSlice";
 import cartReducer from "../slice/cart/cartSlice";
 import orderReducer from "../slice/order";
 import userReducer from "../slice/userInfo";
 import catalogReducer from "../slice/catalog";
+import { persistReducer, persistStore } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  cartInfo: cartReducer,
+  order: orderReducer,
+  user: userReducer,
+  catalog: catalogReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    cartInfo: cartReducer,
-    order: orderReducer,
-    user: userReducer,
-    catalog: catalogReducer,
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
